9046c6593155e200dbac354abc418773e4cfe624
rsullivan2704 <rsullivan2704@gmail.com>
2018-08-23 22:01:03 -0400

    finished implementation

--- SecureList.py
+++ SecureList.py
@@ -3,35 +3,45 @@ from collections.abc import MutableSequence
 class SecureList(MutableSequence):
   
   def __init__(self, data=None, *args):
-    # super(SecureList, self).__init__()
     self._list = list(data) if data else list()
 
   def __getitem__(self, index):
     try:
-      return self._list[index]
-      self.pop(index)
+      item = self._list[index]
+      self._list.pop(index)
     except:
       raise
+    return item
 
   def __str__(self):
     try:
-      return str(self._list)
+      result = str(self._list)
       self._list = list()
     except:
       raise
+    return result
 
   def __repr__(self):
-    try:
-      return self.__str__()
-      self._list = list()
-    except:
-      raise
+    return self.__str__()
 
   def __len__(self):
     return len(self._list)
 
+  def __delitem__(self, value):
+    self._list.remove(value)
+
+  def __setitem__(self, index, value):
+    self._list.insert(index, value)
+
+  def insert(self, index, value):
+    self.__setitem__(index, value)
+
 
 base=[1,2,3,4]
 a=SecureList(base)
+b=SecureList(None)
 
 print(a[0])
+print(a)
+
+print(b)
