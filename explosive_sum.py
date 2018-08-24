2b46bbd7e278552966474c19b688391a4177a8ce
rsullivan2704 <rsullivan2704@gmail.com>
2018-08-24 09:46:11 -0400

    initial commit

---
+++ explosive_sum.py
@@ -0,0 +1,12 @@
+
+
+def exp_sum(n):
+  partitions = []
+
+
+print(exp_sum(1)) #, 1)
+print(exp_sum(2)) #, 2)
+print(exp_sum(3)) #, 3)
+print(exp_sum(4)) #, 5)
+print(exp_sum(5)) #, 7)
+print(exp_sum(10)) #, 42)
